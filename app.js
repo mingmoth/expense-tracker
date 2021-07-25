5bb53dd10c12133c2f6ea51a7eba4c307d431a71
mingmoth <mingmoth@gmail.com>
2021-07-25 14:48:28 +0800

    feat: add category select function

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ db.once('open', () => {
   console.log('mongodb connected')
 })
 
-const Expense = require('./models/expense')
+const Expense = require('./models/expense');
+const expense = require('./models/expense');
 
 
 app.engine('hbs', exphbs({
@@ -49,9 +50,38 @@ app.get('/', (req, res) => {
     .sort({ _id: 'asc' })
     .then((expenses) => {
       expenses.forEach((expense) => totalCost += expense.cost),
-      res.render('index', { expenses, categories, totalCost })
+        res.render('index', { expenses, categories, totalCost })
     })
     .catch(error => console.log(error))
+})
+
+//特定類別支出
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  // console.log(keyword)
+
+  const categories = {
+    家居物業: '<i class="fas fa-laptop-house"></i>',
+    交通出行: '<i class="fas fa-bus-alt"></i>',
+    休閒娛樂: '<i class="fas fa-icons"></i>',
+    餐飲食品: '<i class="fas fa-hotdog"></i>',
+    其他: '<i class="fas fa-paste"></i>'
+  }
+
+  let totalCost = 0
+
+  Expense.find()
+    .lean()
+    .then((expenses) => {
+      if (keyword === '全部') {
+        expenses = expenses
+      } else {
+        expenses = expenses.filter((expense) => expense.category.includes(keyword))
+      }
+      // console.log(expenses)
+      expenses.forEach((expense) => totalCost += expense.cost)
+      res.render('index', { expenses, categories, totalCost, keyword })
+    })
 })
 
 
