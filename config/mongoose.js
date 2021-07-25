36e31bbf52cf1e3ad946f3e1d429e3c659081167
mingmoth <mingmoth@gmail.com>
2021-07-25 17:45:25 +0800

    feat: modify heroku mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/expense-tracker',
-  //MongoDB 的 3.1.0 版之前，向資料庫連線時不一定要加上 port，但在 3.1.0 版本後，連線資料庫時一定要加上 port。
-  //連線 MongoDB 時傳入 { useNewUrlParser: true } 的設定
-  //連線 MongoDB 時傳入 { useUnifiedTopology: true } 的設定
-  { useNewUrlParser: true, useUnifiedTopology: true })
+// mongoose.connect('mongodb://localhost/expense-tracker',
+//   //MongoDB 的 3.1.0 版之前，向資料庫連線時不一定要加上 port，但在 3.1.0 版本後，連線資料庫時一定要加上 port。
+//   //連線 MongoDB 時傳入 { useNewUrlParser: true } 的設定
+//   //連線 MongoDB 時傳入 { useUnifiedTopology: true } 的設定
+//   { useNewUrlParser: true, useUnifiedTopology: true })
 
 // 否則為本地環境，使用 mongodb://localhost/todo-list
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/expense-tracker'
