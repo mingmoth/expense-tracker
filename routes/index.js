d7638ffce8391237e21e9089a061675cd9c96e73
mingmoth <mingmoth@gmail.com>
2021-09-07 22:22:19 +0800

    modify index.hbs layout

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const expenses = require('./modules/expenses')
 // 引入 home 模組程式碼
 const home = require('./modules/home')
+const expenses = require('./modules/expenses')
 
 // 將網址結構符合 / 字串的 request 導向 home 模組
 router.use('/', home)
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const Expense = require('../../models/expense')
 const Category = require('../../models/category')
 const moment = require('moment')
-const { getAccountingFormat, getTotalAmount, getIconClassName } = require('../../tools/helpers')
+const { getIconClassName } = require('../../tools/helpers')
 
 
 
@@ -26,27 +26,12 @@ router.get('/', (req, res) => {
       res.render('index', { expenses, categories, totalCost, startDate: defaultStartDate, endDate: today })
     })
     .catch(err => console.log(err))
-  // try {
-  //   const categories = await Category.find().lean()
-  //   const categoryData = {}
-  //   categories.forEach((category) => {
-  //     categoryData[category.name] = category.icon
-  //   })
 
-  //   const expenses = await Expense.find().sort({ date: 'asc' }).lean()
-
-  //   let totalCost = 0
-
-  //   expenses.forEach((expense) => {
-  //     expense.categoryIcon = categoryData[expense.category]
-  //     totalCost += expense.cost
-  //   })
-  //   return res.render('index', { expenses, categories, totalCost })
-  // } catch (err) { console.log(err) }
 })
 
 //特定類別支出
 router.get('/search', (req, res) => {
+
   const categorySearch = req.query.categorySearch
   let { startDate, endDate } = req.query
   startDate = startDate || '2021-01-01' // default start date
@@ -54,13 +39,15 @@ router.get('/search', (req, res) => {
   if ((new Date(startDate)).valueOf() > new Date(endDate).valueOf()) {
     [startDate, endDate] = [endDate, startDate]
   }
+  
+  console.log(startDate, endDate)
 
   Promise.all([Expense.find({ category: { $regex: categorySearch }, date: { $gte: startDate, $lt: endDate } }).lean().sort('-date'), Category.find().lean()])
     .then(results => {
       const [filteredRecords, categories] = results
-      // const totalCost = getAccountingFormat(getTotalAmount(filteredRecords))
       let totalCost = 0
       filteredRecords.forEach(expense => {
+        console.log(expense)
         expense.categoryIcon = getIconClassName(expense.category, categories)
         expense.date = moment(expense.date).format('YYYY-MM-DD')
         totalCost += expense.cost
@@ -68,34 +55,6 @@ router.get('/search', (req, res) => {
       res.render('index', { expenses: filteredRecords, totalCost, categorySearch, startDate, endDate })
     })
     .catch(err => console.log(err))
-
-  // try {
-  //   const categorySearch = req.query.categorySearch
-  //   const categories = await Category.find().lean()
-
-  //   const filterQuery = {}
-  //   categorySearch ? filterQuery.category = categorySearch : ''
-  //   const expenses = await Expense.aggregate([
-  //     { $project: { name: 1, category: 1, date: 1, cost: 1, comment: 1 } },
-  //     { $match: filterQuery }
-  //   ])
-
-  //   const categoryData = {}
-  //   categories.forEach((category) => {
-  //     categoryData[category.name] = category.icon
-  //   })
-
-  //   let totalCost = 0
-
-  //   expenses.forEach((expense) => {
-  //     expense.categoryIcon = categoryData[expense.category]
-  //     totalCost += expense.cost
-  //   })
-  //   return res.render('index', {
-  //     expenses, categories, totalCost, categorySearch
-  //   })
-
-  // } catch (err) { console.log(err) }
 
 })
 
